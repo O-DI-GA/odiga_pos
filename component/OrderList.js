3b6e2a88c06a4c2b420a9a064cf1df5324ef4946
daj3on9 <dajung5623@naver.com>
2024-10-03 17:01:49 +0900

    :sparkles: Feat : 메인에 테이블 및 웨이팅 현황 추가

---
+++ component/OrderList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View} from "react-native";
+import {Text} from "react-native";
+
+const OrderList = () => {
+    return (
+        <View>
+            <Text> 주문목록 </Text>
+        </View>
+    )
+}
+
+export default OrderList;
