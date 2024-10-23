9b7e551423625f1cdfa746d7e46caabf19357993
daj3on9 <dajung5623@naver.com>
2024-10-23 10:15:27 +0900

    💄 Design : 웨이팅현황 화면 css 수정

--- screens/WaitingList.js
+++ screens/WaitingList.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Pressable, Modal, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Pressable,
+  Modal,
+  FlatList,
+} from "react-native";
 import { StyleSheet } from "react-native";
 import OrderList from "../component/OrderList";
 
 const WaitingList = () => {
-  const [orderInfo, setOrderInfo] = React.useState([{ currentNumber: 103, waitingPeople: 5 }]);
-
+  const [orderInfo, setOrderInfo] = React.useState([
+    { currentNumber: 103, waitingPeople: 5 },
+  ]);
   const [waitingList, setWaitingList] = React.useState([
     {
       waitingId: 1,
       waitingNum: 104,
-      resName: "김형준 님",
-      peopleCount: 3,
+      resName: "김형준",
+      peopleCount: 2,
       menu: [
         { menu: "햄버거", count: 10 },
         { menu: "감자튀김", count: 10 },
@@ -20,11 +28,22 @@ const WaitingList = () => {
     {
       waitingId: 2,
       waitingNum: 105,
-      resName: "구자현 님",
-      peopleCount: 3,
+      resName: "구자현",
+      peopleCount: 1,
       menu: [
         { menu: "치킨버거", count: 1 },
         { menu: "양념감자", count: 1 },
+        { menu: "콜라", count: 1 },
+      ],
+    },
+    {
+      waitingId: 3,
+      waitingNum: 106,
+      resName: "김건택",
+      peopleCount: 1,
+      menu: [
+        { menu: "싸이버거", count: 2 },
+        { menu: "감자튀김", count: 1 },
         { menu: "콜라", count: 1 },
       ],
     },
@@ -33,61 +52,71 @@ const WaitingList = () => {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
   const [selectedMenu, setSelectedMenu] = React.useState([]);
 
+  // 모달 오픈
   const onPressModalOpen = (menu) => {
     setSelectedMenu(menu);
     setIsModalOpen(true);
   };
 
+  // 모달 닫기
   const onPressModalClose = () => {
     setIsModalOpen(false);
   };
 
+  // 메뉴 모달
   const renderModalContent = () => {
     return (
-        <Modal visible={isModalOpen} transparent={true} animationType="slide">
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <Text style={styles.modalTitle}>메뉴</Text>
-              {selectedMenu.map((item, index) => (
-                  <View key={index} style={styles.modalItem}>
-                    <Text>{item.menu}</Text>
-                    <Text>{item.count}</Text>
-                  </View>
-              ))}
-              <TouchableOpacity onPress={onPressModalClose} style={styles.closeButton}>
-                <Text style={styles.closeButtonText}>확인</Text>
-              </TouchableOpacity>
-            </View>
+      <Modal visible={isModalOpen} transparent={true} animationType="slide">
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalTitle}>주문 내역</Text>
+            {selectedMenu.map((item, index) => (
+              <View key={index} style={styles.modalItem}>
+                <Text>{item.menu}</Text>
+                <Text>{item.count}</Text>
+              </View>
+            ))}
+            <TouchableOpacity
+              onPress={onPressModalClose}
+              style={styles.closeButton}
+            >
+              <Text style={styles.closeButtonText}>확인</Text>
+            </TouchableOpacity>
           </View>
-        </Modal>
+        </View>
+      </Modal>
     );
   };
 
   const renderWaitingItem = ({ item }) => {
     return (
-        <View style={styles.waitingItem}>
-          <View style={styles.waitingInfo}>
-            <Text>{item.waitingId}</Text>
-            <Text>{item.resName}</Text>
-            <Text>{item.peopleCount}명</Text>
+      <View style={styles.waitingItem}>
+        <View style={styles.waitingInfo}>
+          <View style={styles.waitingNumber}>
+            <Text>{item.waitingNum}</Text>
           </View>
-          <Pressable onPress={() => onPressModalOpen(item.menu)} style={styles.menuButton}>
+          <Text>{item.resName}님</Text>
+        </View>
+        <View style={styles.waitingBtnBox}>
+          <Text>{item.peopleCount}명</Text>
+          <Pressable
+            onPress={() => onPressModalOpen(item.menu)}
+            style={styles.menuButton}
+          >
             <Text style={styles.menuButtonText}>메뉴보기</Text>
           </Pressable>
           <TouchableOpacity style={styles.cancelButton}>
             <Text style={styles.cancelButtonText}>취소</Text>
           </TouchableOpacity>
         </View>
+      </View>
     );
   };
 
   return (
-      <View style={styles.container}>
-        <View style={styles.leftBox}>
+    <View style={styles.container}>
+      <View style={styles.leftBox}>
         <View style={styles.header}>
-          <TouchableOpacity>
-            <Text style={styles.backButton}>이전 버튼</Text>
-          </TouchableOpacity>
           <Text style={styles.title}>웨이팅 현황</Text>
         </View>
         <View style={styles.status}>
@@ -95,16 +124,16 @@ const WaitingList = () => {
           <Text>현재 번호 : {orderInfo[0].currentNumber}번</Text>
         </View>
         <FlatList
-            data={waitingList}
-            renderItem={renderWaitingItem}
-            keyExtractor={(item) => item.waitingId.toString()}
+          data={waitingList}
+          renderItem={renderWaitingItem}
+          keyExtractor={(item) => item.waitingId.toString()}
         />
         {renderModalContent()}
-        </View>
-        <View style={styles.rightBox}>
-          <OrderList/>
-        </View>
       </View>
+      <View style={styles.rightBox}>
+        <OrderList />
+      </View>
+    </View>
   );
 };
 
@@ -114,94 +143,118 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#F7F5F0",
-    flexDirection : "row",
+    flexDirection: "row",
   },
   leftBox: {
-    flex : 4,
-    padding : 15,
+    flex: 4,
+    padding: 20,
   },
   rightBox: {
-    flex : 1,
+    flex: 1,
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
+    flexDirection: "row",
+    justifyContent: "flex-start",
     marginBottom: 10,
   },
   backButton: {
-    color: '#333',
+    color: "#333",
     fontSize: 16,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   status: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 20,
     marginBottom: 20,
   },
+  // 웨이팅 정보
   waitingItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    padding: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#fff",
     marginBottom: 10,
     borderRadius: 10,
     elevation: 1,
+    height: 80,
   },
   waitingInfo: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    height: "100%",
+    gap: 20,
+  },
+  waitingNumber: {
+    display: "flex",
+    backgroundColor: "#FFF1CE",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    padding: 25,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+  waitingBtnBox: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: "100%",
+    gap: 50,
+    marginRight: 30,
   },
   menuButton: {
-    backgroundColor: '#d9d9d9',
     paddingVertical: 5,
     paddingHorizontal: 10,
     borderRadius: 5,
   },
   menuButtonText: {
-    color: '#333',
+    color: "#9D9D9D",
   },
   cancelButton: {
-    backgroundColor: '#ff5c5c',
+    backgroundColor: "#ff5c5c",
     paddingVertical: 5,
-    paddingHorizontal: 10,
+    paddingHorizontal: 15,
     borderRadius: 5,
   },
   cancelButtonText: {
-    color: '#fff',
+    color: "#fff",
   },
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
   },
   modalView: {
     width: 300,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20,
-    alignItems: 'center',
+    alignItems: "center",
   },
   modalTitle: {
     fontSize: 18,
-    marginBottom: 15,
+    marginBottom: 30,
   },
   modalItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
     marginBottom: 10,
   },
   closeButton: {
-    marginTop: 20,
-    backgroundColor: '#ff5c5c',
-    padding: 10,
+    marginTop: 25,
+    backgroundColor: "#ff5c5c",
+    paddingVertical: 5,
+    paddingHorizontal: 20,
     borderRadius: 5,
   },
   closeButtonText: {
-    color: '#fff',
+    color: "#fff",
   },
 });
 
