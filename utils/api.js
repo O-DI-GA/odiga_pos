f1a95b0ce5974a14a0e972226e40c0741f304a20
hjoo830 <hjoo830@naver.com>
2024-11-01 23:26:20 +0900

    ✨ Feat: Add deleteDataRequest for DELETE with data

--- utils/api.js
+++ utils/api.js
@@ -101,4 +101,21 @@ export const deleteRequest = async (endpoint) => {
   }
 };
 
+// DELETE with data
+export const deleteDataRequest = async (endpoint, data) => {
+  try {
+    const response = await fetch(`${URL}/${endpoint}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
+
 export default URL;
